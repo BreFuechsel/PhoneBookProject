4c5a82412721d83f58712ea0e533e381a69e7144
Bre Fuechsel <brefuechsel@gmail.com>
2021-03-23 14:36:55 -0700

    Initial commit of working PhoneBook project

---
+++ PhoneBook/src/PhoneBook/PhoneBook.java
@@ -0,0 +1,30 @@
+package PhoneBook;
+
+import java.util.*;
+import Contact.Contact;
+
+public class PhoneBook extends Contact{
+	
+	//Object phoneBook;
+	Long deletePhoneNumber;
+	String emailDelete;
+	String phoneBook;
+	String deleteFirstName;
+	String deleteLastName;
+	String deleteCity;
+	String deleteState;
+	Long searchPhoneNumber;
+	String searchEmail;
+	String searchFirstName;
+	String searchLastName;
+	String searchCity;
+	String searchState;
+	
+	
+	
+	
+	
+	
+
+} 
+    
